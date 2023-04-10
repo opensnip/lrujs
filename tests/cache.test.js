0458fa9f08b9d256d21d095f9f5f74b50205cdd7
Rajkumar Dusad <32831684+rajkumardusad@users.noreply.github.com>
2023-04-10 11:42:37 +0530

    Update cache.test.js

--- tests/cache.test.js
+++ tests/cache.test.js
@@ -71,7 +71,7 @@ describe("LRU Cache test", () => {
     cache.set("c", 30);
     cache.set("d", 40);
     cache.set("e", 50);
-    expect(cache.toArray()).toEqual([
+    expect(cache.toArray()).toMatchArray([
       { e: 50 },
       { d: 40 },
       { c: 30 },
@@ -86,7 +86,7 @@ describe("LRU Cache test", () => {
     cache.set("d", 40);
     cache.get("a");
     cache.set("e", 50);
-    expect(cache.toArray()).toEqual([
+    expect(cache.toArray()).toMatchArray([
       { e: 50 },
       { a: 10 },
       { d: 40 },
@@ -101,7 +101,7 @@ describe("LRU Cache test", () => {
     cache.set("d", 40);
     cache.get("d");
     cache.set("e", 50);
-    expect(cache.toArray()).toEqual([
+    expect(cache.toArray()).toMatchArray([
       { e: 50 },
       { d: 40 },
       { c: 30 },
